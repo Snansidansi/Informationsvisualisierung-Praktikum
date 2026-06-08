f3355b226bf3f3f3848f6122a94efbf13357f67e
Snansidansi <165903080+Snansidansi@users.noreply.github.com>
2026-06-08 18:43:00 +0200

    Abgabe 2: umschalte für lineare regression und clustering

--- Abgabe-2/main.py
+++ Abgabe-2/main.py
@@ -2,10 +2,10 @@ import html
 import sys
 import lineare_regression
 import dash_bootstrap_components as dbc
-from dash import dcc
+from dash import dcc, html, callback, Input, Output
 
 from datenbereinigung import clean_data
-from dash import Dash, html
+from dash import Dash
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
 
@@ -19,9 +19,12 @@ if __name__ == "__main__":
     cleaned_data = clean_data(csvPath, True)
 
     app.layout = html.Div([
-        html.H1(children = "Abgabe 2 - Regression und Clustering"),
+        html.H1(children="Abgabe 2 - Regression und Clustering", style={'marginBottom': '30px'}),
         dcc.Store(id="data-store", data=cleaned_data.to_dict("records")),
-        lineare_regression.doRegression(cleaned_data)
-    ])
+        dcc.Tabs([
+            dcc.Tab(label="Lineare Regression", children=html.Div(lineare_regression.doRegression(cleaned_data), style={'padding': '20px'})),
+            dcc.Tab(label="K-Means Clustering", children=html.Div(style={'padding': '20px'}))
+        ], style={'padding': '20px'})
+    ], style={'padding': '20px'})
 
     app.run(debug=True)
